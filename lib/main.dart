286e60ec3d07def6180719cb033774ec317541cf
firmanalamanta <firmanalamanta2001@gmail.com>
2022-09-15 14:36:55 +0700

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:provider/provider.dart';
 import 'firebase_options.dart';         // new
 import 'src/authentication.dart';                  // new
 import 'src/widgets.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
     runApp(
@@ -53,7 +55,6 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
-          // Add from here
           Consumer<ApplicationState>(
             builder: (context, appState, _) => Authentication(
               email: appState.email,
@@ -66,7 +67,6 @@ class HomePage extends StatelessWidget {
               signOut: appState.signOut,
             ),
           ),
-          // to here
           const Divider(
             height: 8,
             thickness: 1,
@@ -78,6 +78,21 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+          // Modify from here
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loginState == ApplicationLoginState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
+          ),  
         ],
       ),
     );
@@ -87,6 +102,21 @@ class HomePage extends StatelessWidget {
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
+  }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (_loginState != ApplicationLoginState.loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
   }
 
   Future<void> init() async {
@@ -170,5 +200,66 @@ class ApplicationState extends ChangeNotifier {
 
   void signOut() {
     FirebaseAuth.instance.signOut();
+  }
+  
+}
+
+class GuestBook extends StatefulWidget {
+  const GuestBook({required this.addMessage});
+  //final FutureOr<void> Function(String message) addMessage;
+  final void Function(String message) addMessage;
+
+
+  @override
+  _GuestBookState createState() => _GuestBookState();
+}
+
+class _GuestBookState extends State<GuestBook> {
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  hintText: 'Leave a message',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter your message to continue';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(width: 8),
+            StyledButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  //await widget.addMessage(_controller.text);
+                  widget.addMessage(_controller.text);
+                  _controller.clear();
+                }
+              },
+              child: Row(
+                children: const [
+                  Icon(Icons.send),
+                  SizedBox(width: 4),
+                  Text('SEND'),
+                ],
+              ),
+           ),
+          ],
+        ),
+      ),
+    );
   }
 }
